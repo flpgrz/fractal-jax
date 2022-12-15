1a2a2f24e96286797d90a6b050b5b2cdad0362a0
Filippo Grazioli <sendtofilippo@gmail.com>
2022-12-15 17:50:41 +0100

    upd version

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ install_requires = [
 
 setup(
     name='fractal_jax',
-    version='0.1.0',
+    version='0.1.1',
     description=description(),
     author="Filippo Grazioli",
     author_email="sendtofilippo@gmail.com",
